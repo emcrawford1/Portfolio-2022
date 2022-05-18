e9e9c0d30eb9f92ae22cbc79b4bcc347a9b12a4c
mrCrawford <noreply>
2022-05-18 01:00:50 -0500

    Added rotating carousel functionality and worked on Contact form.  Will need to add backend logic so that sending information via form will email me directly.  Also, might need to add github and linkedin links.

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -7,7 +7,6 @@ const rightArrowImg = document.getElementById("right-arrow");
 const leftArrowImg = document.getElementById("left-arrow");
 const mainContainer = document.getElementById("main-container");
 
-
 //Project listing for carousel cards
 const projects = [
   {
@@ -40,54 +39,47 @@ const projects = [
     pic: "./assets/images/planit.jpg",
     github: "https://github.com/emcrawford1/Project1",
   },
-  {
-    name: "Wisdom Threads",
-    description:
-      "This web app provides users of all ages and walks of life a platform for sharing wisdom, useful tips, and experiences.",
-    pic: "./assets/images/wisdom_threads.png",
-    deployed: "https://storythread.herokuapp.com",
-    github: "https://github.com/emcrawford1/project2",
-  },
+  // {
+  //   name: "Wisdom Threads",
+  //   description:
+  //     "This web app provides users of all ages and walks of life a platform for sharing wisdom, useful tips, and experiences.",
+  //   pic: "./assets/images/wisdom_threads.png",
+  //   deployed: "https://storythread.herokuapp.com",
+  //   github: "https://github.com/emcrawford1/project2",
+  // },
 ];
 
 const cycleRight = () => {
   const currentPressTime = new Date();
-  let clickInterval;
 
   //Only allow button to be responsive when animation is not in transition
   if (!hoverPause) {
-    const imgContainer = document.getElementById("img-container");
-    const infoNode = document.getElementsByClassName("information")[0];
+    const spokes = document.getElementsByClassName("spoke");
+    const blocks = document.getElementsByClassName("block");
 
-    imgContainer.classList.remove("img-autocycle");
-    infoNode.classList.remove("information-autocycle");
-
-    imgContainer.classList.add("img-slidein-left");
-    infoNode.classList.add("information-leftcycle");
-
-    //Get the time interval for the last time the user clicked the button
+    //Get the amount of time in between clicks
     clickInterval = currentPressTime - lastPressTime;
 
-    //Add the appropriate CSS class (each has a different animation-duration)
-    switch (true) {
-      case clickInterval <= 200:
-        imgContainer.classList.add("img-fast-cycle");
-        infoNode.classList.add("information-fast-cycle");
-        break;
+    for (let i = 0; i < spokes.length; i++)
+      spokes[i].classList.remove(
+        "img-autocycle",
+        `spoke-${i + 1}r`,
+        `spoke-${i + 1}l`
+      );
+    for (let i = 0; i < blocks.length; i++)
+      blocks[i].classList.remove(
+        "img-autocycle",
+        `block-${i + 1}r`,
+        `block-${i + 1}l`
+      );
 
-      case clickInterval > 200 && clickInterval <= 700:
-        imgContainer.classList.add("img-medium-cycle");
-        infoNode.classList.add("information-medium-cycle");
-        break;
+    void leftArrowImg.offsetHeight;
 
-      default:
-        imgContainer.classList.add("img-normal-cycle");
-        infoNode.classList.add("information-normal-cycle");
-    }
-
-    void infoNode.offsetWidth;
+    for (let i = 0; i < spokes.length; i++)
+      spokes[i].classList.add("cycle-once", `spoke-${i + 1}r`, "cycle-speed");
+    for (let i = 0; i < blocks.length; i++)
+      blocks[i].classList.add("cycle-once", `block-${i + 1}r`, "cycle-speed");
   }
-
   //Set lastPressTime variable to the last time the button was clicked
   lastPressTime = new Date();
 };
@@ -95,39 +87,34 @@ const cycleRight = () => {
 const cycleLeft = () => {
   //Variable to check speed in between button clicks
   const currentPressTime = new Date();
+  let clickInterval;
 
   //Only allow button to be responsive when animation is not in transition
   if (!hoverPause) {
-    const imgContainer = document.getElementById("img-container");
-    const infoNode = document.getElementsByClassName("information")[0];
-
-    imgContainer.classList.remove("img-autocycle");
-    infoNode.classList.remove("information-autocycle");
-
-    imgContainer.classList.add("img-slidein-right");
-    infoNode.classList.add("information-rightcycle");
+    const spokes = document.getElementsByClassName("spoke");
+    const blocks = document.getElementsByClassName("block");
 
     //Get the time interval for the last time the user clicked the button
     clickInterval = currentPressTime - lastPressTime;
 
-    //Add the appropriate CSS class (each has a different animation-duration)
-    switch (true) {
-      case clickInterval <= 200:
-        imgContainer.classList.add("img-fast-cycle");
-        infoNode.classList.add("information-fast-cycle");
-        break;
+    for (let i = 0; i < spokes.length; i++)
+      spokes[i].classList.remove(
+        "img-autocycle",
+        `spoke-${i + 1}r`,
+        `spoke-${i + 1}l`
+      );
+    for (let i = 0; i < blocks.length; i++)
+      blocks[i].classList.remove(
+        "img-autocycle",
+        `block-${i + 1}r`,
+        `block-${i + 1}l`
+      );
 
-      case clickInterval > 200 && clickInterval <= 700:
-        imgContainer.classList.add("img-medium-cycle");
-        infoNode.classList.add("information-medium-cycle");
-        break;
+    void leftArrowImg.offsetHeight;
 
-      default:
-        imgContainer.classList.add("img-normal-cycle");
-        infoNode.classList.add("information-normal-cycle");
-    }
+    for (let i = 0; i < spokes.length; i++) spokes[i].classList.add("cycle-once",`spoke-${i + 1}l`,"cycle-speed");
+    for (let i = 0; i < blocks.length; i++) blocks[i].classList.add("cycle-once", `block-${i + 1}l`, "cycle-speed");
 
-    void infoNode.offsetWidth;
   }
 
   //Set lastPressTime variable to the last time the button was clicked
@@ -138,139 +125,181 @@ const cycleLeft = () => {
 //attached to an event listener and hoverPause can be set to false.
 const startCycle = (event) => {
   hoverPause = false;
-  if (event.animationName === "right-cycle") carouselCycle(-1);
+  if (event.animationName === "block-rotate-left-2") carouselCycle(-1);
   else carouselCycle(1);
 };
 
 //Do not freeze carousel while animation is in progress
-const delayPause = () => (hoverPause = true);
+const delayPause = () => {
+  
+  //Remove transition during carousel movement
+  const card = document.getElementsByClassName("portfolio-card")[0];
+  card.classList.remove("portfolio-card");
+  card.classList.add("card-transition");
+  hoverPause = true
+
+};
 
 //Create and return an array of <img> elements for DOM
 const createImage = (portfolio, index) => {
   const imageList = [];
-  for (let i = index; i < index + portfolio.length + 2; i++) {
-    //Add informational node to image array
-    if (
-      i % portfolio.length ===
-      Math.floor((index + portfolio.length / 2 + 1) % portfolio.length)
-    ) {
-      const infNode = document.createElement("div");
 
-      //Create information that will appear in the information panel
-      const projectName = document.createElement("div");
-      const projectDescr = document.createElement("div");
-      const projectGitHub = document.createElement("a");
+  for (let i = index, j = 1; i < index + portfolio.length; i++, j++) {
 
-      projectGitHub.setAttribute(
-        "href",
-        portfolio[i % portfolio.length].github
-      );
+    const spoke = document.createElement("div");
+    const block = document.createElement("div");
+    const img = document.createElement("img");
 
-      projectName.innerHTML = portfolio[i % portfolio.length].name;
-      projectDescr.innerHTML = portfolio[i % portfolio.length].description;
-      projectGitHub.innerHTML = portfolio[i % portfolio.length].github;
+    spoke.classList.add("spoke");
+    spoke.classList.add(`spoke-${j}r`);
+    spoke.classList.add("img-autocycle");
+    block.classList.add("block");
+    block.classList.add(`block-${j}r`);
+    block.classList.add("img-autocycle");
+    img.classList.add("carousel-image");
+    img.src = portfolio[i % portfolio.length].pic;
+    img.alt = portfolio[i % portfolio.length].name;
 
-      //Add containers for anchor tags (for formatting)
-      const githubContainer = document.createElement("div");
-      githubContainer.innerHTML = "GitHub: ";
-      githubContainer.appendChild(projectGitHub);
-
-      //Add classes to the information panel items
-      projectName.classList.add("project-name");
-      projectDescr.classList.add("project-descr");
-      githubContainer.classList.add("project-github");
-
-      infNode.appendChild(projectName);
-      infNode.appendChild(projectDescr);
-      infNode.appendChild(githubContainer);
-
-      //Check if current project object has a deployed link.  If so create and add to information node.
-      if (portfolio[i % portfolio.length].hasOwnProperty("deployed")) {
-        const deployedContainter = document.createElement("div");
-        const projectDeployed = document.createElement("a");
-
-        projectDeployed.setAttribute(
-          "href",
-          portfolio[i % portfolio.length].deployed
-        );
-        projectDeployed.innerHTML = portfolio[i % portfolio.length].deployed;
-
-        deployedContainter.classList.add("project-deployed");
-        deployedContainter.innerHTML = "Deployed: ";
-        deployedContainter.appendChild(projectDeployed);
-
-        infNode.appendChild(deployedContainter);
-      }
-
-      infNode.classList.add("information");
-      infNode.classList.add("information-autocycle");
-
-      //Add eventlisteners
-      infNode.addEventListener("animationend", startCycle);
-      infNode.addEventListener("animationstart", delayPause);
-      infNode.addEventListener("mouseenter", pauseCarousel);
-      infNode.addEventListener("mouseleave", resumeCarousel);
-
-      const focusImg = document.createElement("img");
-      const containerDiv = document.createElement("div");
-      focusImg.src = portfolio[i % portfolio.length].pic;
-      focusImg.alt = portfolio[i % portfolio.length].name;
-
-      //Add event listeners to freeze & resume carousel when mouse enters & exits slide
-      focusImg.addEventListener("mouseenter", pauseCarousel);
-      focusImg.addEventListener("mouseleave", resumeCarousel);
-
-      focusImg.classList.add("focus-image");
-
-      containerDiv.classList.add("img-container-div");
-      containerDiv.appendChild(focusImg);
-
-      imageList.push(infNode);
-      imageList.push(containerDiv);
-    } else {
-      const imgNode = document.createElement("img");
-      imgNode.src = portfolio[i % portfolio.length].pic;
-      imgNode.alt = portfolio[i % portfolio.length].name;
-      imgNode.classList.add("side-images");
-      imageList.push(imgNode);
+    if (j === 2) {
+      const card = createCard(portfolio, img, i);
+      block.classList.add("focus-slide");
+      spoke.classList.add("focus-slide");
+      block.addEventListener("animationend", startCycle);
+      block.addEventListener("animationstart", delayPause);
+      block.addEventListener("mouseenter", pauseCarousel);
+      block.addEventListener("mouseleave", resumeCarousel);
+      block.appendChild(card);
     }
+
+    else block.appendChild(img)
+
+    spoke.appendChild(block);
+
+    imageList.push(spoke);
   }
   return imageList;
 };
 
+const createIndicators = (portfolio, currentPosition) => {
+  
+  const indicators = []
+
+  //The focus element is technically the second item in the array; however, since it is the "focus element" we 
+  //need for it to appear as the first item in the indicator listing.   
+  const focusPosition = ((currentPosition - 1) + portfolio.length) % portfolio.length;
+
+  for(let i = 0; i < portfolio.length; i++) {
+    const iElement = document.createElement('div');
+
+    if(i === focusPosition) iElement.classList.add('current-indicator');
+    else iElement.classList.add('blank-indicator');
+
+    indicators.push(iElement)
+  }
+  return indicators;
+}
+
+//Create rotating informational card (this will be the focus slide in the carousel)
+const createCard = (portfolio, imgElement, index) => {
+
+  const card = document.createElement('div');
+  const cardInner = document.createElement('div');
+  const cardFront = document.createElement('div');
+  
+  //Project information
+  const cardBack = document.createElement('div');
+  const infoDescription = document.createElement('div');
+  const infoTitle = document.createElement('div');
+  const githubContainer = document.createElement('div');
+  const githubLink = document.createElement('a');
+ 
+
+  //Add classes
+  card.classList.add('portfolio-card');
+  cardInner.classList.add('card-inner');
+  cardFront.classList.add('card-front');
+  cardBack.classList.add('card-back');
+  infoTitle.classList.add('info-title');
+  infoDescription.classList.add('info-description');
+  githubContainer.classList.add('github-container');
+
+  githubLink.classList.add('github-link');
+
+  imgElement.classList.add("rotate-image");
+
+  //Add information to back of card
+  infoDescription.innerHTML = portfolio[index%portfolio.length].description;
+  infoTitle.innerHTML = portfolio[index%portfolio.length].name;
+  githubContainer.innerHTML = "GitHub: "
+  githubLink.setAttribute("href", portfolio[index%portfolio.length].github);
+  githubLink.setAttribute("target","_blank");
+  githubLink.innerHTML = portfolio[index%portfolio.length].github;
+
+
+  //Build back of card
+  cardBack.appendChild(infoTitle);
+  cardBack.appendChild(infoDescription);
+  cardBack.appendChild(githubContainer);
+
+  //Check to see if the current project has a deployed link.  If so add to card
+  if(portfolio[index % portfolio.length].hasOwnProperty("deployed")) {
+    const deployedContainer = document.createElement('div');
+    const deployedLink = document.createElement('a');
+
+    deployedContainer.classList.add('deployed-container');
+    deployedLink.classList.add('deployed-link');
+
+    deployedLink.setAttribute("href", portfolio[index%portfolio.length].deployed);
+    deployedLink.setAttribute("target","_blank");
+
+    deployedContainer.innerHTML = "Deployed: "
+    deployedLink.innerHTML = portfolio[index%portfolio.length].deployed;
+
+    deployedContainer.appendChild(deployedLink);
+    cardBack.appendChild(deployedContainer);
+  }
+
+  //Build rest of card
+  githubContainer.appendChild(githubLink);
+  cardFront.appendChild(imgElement);
+  cardInner.appendChild(cardFront);
+  cardInner.appendChild(cardBack);
+  card.appendChild(cardInner);
+
+  return card;
+}
+
 //Remove all <img> elements so that they can be replaced
 const resetContainer = () => {
-  const removeNode = document.getElementById("img-container");
+
+  const removeNode = document.getElementById("carousel-items");
+  const carousel = document.getElementById("carousel");
+  const focusSlide = document.getElementsByClassName("focus-slide")[0];
+
+  const carouselItems = document.createElement("div");
   const imgContainer = document.createElement("div");
-  const infoSlide = document.getElementsByClassName("information")[0];
-  const focusSlide = document.getElementsByClassName("focus-image")[0];
-
-  //Remove img-container element from the main container element
-  mainContainer.removeChild(removeNode);
-
-  //Add id to new image container element
-  imgContainer.id = "img-container";
-
-  //Add img-autocycle class to element that will hold images.  This class has an animation attached that will
-  //cycle through the elements
-  imgContainer.classList.add("img-autocycle");
-
-  //Attach new image container element to main container element
-  mainContainer.appendChild(imgContainer);
-
-  //Remove event listeners if infoSlide node exists
-  if (infoSlide) {
-    infoSlide.removeEventListener("animationend", startCycle);
-    infoSlide.removeEventListener("animationstart", delayPause);
-    infoSlide.removeEventListener("mouseenter", pauseCarousel);
-    infoSlide.removeEventListener("mouseleave", resumeCarousel);
-  }
+  const indicatorContainer = document.createElement("div");
 
   //Remove event listeners if focusSlide node exists
   if (focusSlide) {
+    focusSlide.removeEventListener("animationend", startCycle);
+    focusSlide.removeEventListener("animationstart", delayPause);
     focusSlide.removeEventListener("mouseenter", pauseCarousel);
     focusSlide.removeEventListener("mouseleave", resumeCarousel);
   }
+
+  //Remove current carousel items from carousel element
+  carousel.removeChild(removeNode);
+
+  //Add id & classnames to elements
+  carouselItems.id = "carousel-items";
+  imgContainer.id = "img-container";
+  indicatorContainer.id = "indicator-container";
+  
+  //Build carousel
+  carouselItems.appendChild(imgContainer);
+  carouselItems.appendChild(indicatorContainer);
+  carousel.appendChild(carouselItems);
 
   return document.getElementById("img-container");
 };
@@ -278,6 +307,7 @@ const resetContainer = () => {
 //Carousel cycling function
 const carouselCycle = (incrementor) => {
   const imgContainer = resetContainer();
+  const indicatorContainer = document.getElementById("indicator-container");
   position = position + incrementor;
 
   //Ensure that a postive increment and a negative decrement both correspond to the appropriate values
@@ -286,42 +316,38 @@ const carouselCycle = (incrementor) => {
   position = ((position % projects.length) + projects.length) % projects.length;
 
   const imageNodes = createImage(projects, position);
+  const indicators = createIndicators(projects, position);
+
+  indicators.forEach((node) => indicatorContainer.appendChild(node));
   imageNodes.forEach((node) => imgContainer.appendChild(node));
 };
 
+//Pause carousel if user hovers over focused slide
 const pauseCarousel = () => {
   if (!hoverPause) {
-    document.getElementsByClassName("information")[0].style.animationPlayState =
-      "paused";
-    document.getElementById("img-container").style.animationPlayState =
-      "paused";
+    const spokes = document.getElementsByClassName("spoke");
+    const blocks = document.getElementsByClassName("block");
+
+    for (let i = 0; i < spokes.length; i++)
+      spokes[i].style.animationPlayState = "paused";
+    for (let i = 0; i < blocks.length; i++)
+      blocks[i].style.animationPlayState = "paused";
   }
 };
 
+//Resume carousel if user moves mouse out of focused slide
 const resumeCarousel = () => {
-  document.getElementsByClassName("information")[0].style.animationPlayState =
-    "running";
-  document.getElementById("img-container").style.animationPlayState = "running";
+  const spokes = document.getElementsByClassName("spoke");
+  const blocks = document.getElementsByClassName("block");
+
+  for (let i = 0; i < spokes.length; i++)
+    spokes[i].style.animationPlayState = "running";
+  for (let i = 0; i < blocks.length; i++)
+    blocks[i].style.animationPlayState = "running";
 };
 
 //Start carousel
 carouselCycle(1);
-
-//Find and set width for transparent blurring divs
-const windowResize = () => {
-  const blurRight = document.getElementById("blur-right");
-  blurRight.style.width = `${(window.innerWidth - 450) / 2}px`;
-
-  const blurLeft = document.getElementById("blur-left");
-  blurLeft.style.width = `${(window.innerWidth - 450) / 2}px`;
-};
-
-
-//Run windowResize function to set initial values of the blur divs
-windowResize();
-
-//Attach event listener to window object so that if the window is resized the applicable divs are resized
-window.addEventListener("resize", windowResize);
 
 rightArrowImg.addEventListener("click", cycleRight);
 leftArrowImg.addEventListener("click", cycleLeft);
